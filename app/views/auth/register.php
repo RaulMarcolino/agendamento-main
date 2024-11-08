7f435a9653c053c2fee1c2d2cb65fe8fe81fb41e
Raul <luiz.junior53@aluno.ce.gov.br>
2024-11-08 12:01:55 -0300

    segundo commit

--- app/views/auth/register.php
+++ app/views/auth/register.php
@@ -8,32 +8,32 @@
   <title>Registro</title>
 </head>
 
-<body class="bg-white-900 text-black flex items-center justify-center min-h-screen">
-  <div class="bg-gray-200 p-8 rounded-lg shadow-md w-full max-w-sm">
-    <h2 class="text-2xl font-semibold text-center text-gray-700 mb-6">Registrar-se</h2>
+<body class="bg-amber-50 text-black flex items-center justify-center min-h-screen">
+  <div class="bg-amber-100 p-8 rounded-lg shadow-md w-full max-w-sm">
+    <h2 class="text-2xl font-semibold text-center text-black mb-6">Registrar-se</h2>
     <form action="/register" method="POST">
       <div class="mb-4">
-        <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
+        <label for="name" class="block text-sm font-medium text-black">Nome</label>
         <input type="text" id="name" name="name" required
           class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
       </div>
       <div class="mb-4">
-        <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+        <label for="email" class="block text-sm font-medium text-black">Email</label>
         <input type="text" id="email" name="email" required
           class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
       </div>
       <div class="mb-4">
-        <label for="Username" class="block text-sm font-medium text-gray-700">Username</label>
+        <label for="Username" class="block text-sm font-medium text-black">Nome de Usuário</label>
         <input type="text" id="username" name="username" required
           class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
       </div>
       <div class="mb-6">
-        <label for="password" class="block text-sm font-medium text-gray-700">Senha</label>
+        <label for="password" class="block text-sm font-medium text-black">Senha</label>
         <input type="password" id="password" name="password" required
           class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
       </div>
       <input type="submit" value="Entrar"
-        class="w-full bg-indigo-600 text-white font-semibold py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50">
+        class="w-full bg-black text-white font-semibold py-2 px-4 rounded-md hover:bg-black focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50">
     </form>
   </div>
 </body>
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -7,8 +7,8 @@
   <script src="https://cdn.tailwindcss.com"></script>
   <title>Agendamento</title>
 </head>
-<body class="bg-white text-white w-full h-full">
-<div class="flex flex-col items-center justify-center bg-white rounded mt-32 p-8">
+<body class="bg-amber-50 text-white w-full h-full">
+<div class="flex flex-col items-center justify-center bg-amber-50 rounded mt-32 p-8">
     <div class="w-full max-w-4xl mx-auto p-8 grid grid-cols-1 md:grid-cols-2 gap-8">
       <div class="flex flex-col justify-center items-center md:items-start text-center md:text-left">
         <h1 class="text-4xl font-bold text-gray-800 mb-4">Agendamentos</h1>
@@ -16,12 +16,12 @@
       </div>
       <div class="flex flex-col justify-center items-center">
         <a href="/login"
-        class="bg-slate-900 text-white font-semibold text-lg py-2 px-4 rounded-full hover:bg-slate-600 w-48 text-center mb-4">
+        class="bg-black text-white font-semibold text-lg py-2 px-4 rounded-full hover:bg-slate-600 w-48 text-center mb-4">
         Fazer Login
       </a>
       <span class="mb-4 text-black">ou</span>
       <a href="/register"
-      class="border border-blue-500 text-blue-500 font-semibold text-lg py-2 px-4 rounded-full hover:bg-blue-50 w-48 text-center">
+      class="bg-white border border-blue-500 text-blue-500 font-semibold text-lg py-2 px-4 rounded-full hover:bg-blue-50 w-48 text-center">
       Cadastrar-se
     </a>
     </div>
